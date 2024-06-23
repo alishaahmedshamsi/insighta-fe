8be820221bcffebd2b2a5960375db3fabec0b3fa
ali usaid <aliusaid55@gmail.com>
2024-06-22 18:46:48 -0700

    feat: Add announcements to teacher dashboard page
    This commit adds the functionality to display announcements on the teacher dashboard page. It includes the following changes:
    - Import the necessary dependencies and functions
    - Define the Announcements component to fetch and display announcements
    - Use the Announcements component in the main page component
    - Update the UI to show the list of previous announcements
    
    The code changes aim to enhance the teacher dashboard by providing a way to view and manage announcements. This feature will improve communication between teachers and students.

--- app/teacher-dashboard/announcement/page.tsx
+++ app/teacher-dashboard/announcement/page.tsx
@@ -1,69 +1,60 @@
 "use client";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
 import DashboardLayout from "@/components/layouts/dashboard.layout";
-import Link from "next/link";
-import { TEACHER_QUICK_START_LIST } from "@/utils/constant/constant";
+import {  TEACHER_QUICK_START_LIST } from "@/utils/constant/constant";
+import {
+  fetchAnnouncements,
+} from "@/services/apis/announcment.api";
+import { useQuery } from "@tanstack/react-query";
+import { useCurrentUser } from "@/hooks/user.hook";
+import { formatDate } from "@/lib/utils";
 import { teacherLeftSidebarLinks } from "@/components/left-sidebar/teacher";
 
-import { date } from "zod";
-
-
-const previousAnnouncements = [
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-	{
-		date: "2022-10-10",
-		announcement: "This is the first announcement",
-	},
-];
-
 export default function Component() {
-	return (
-		<>
-			<DashboardLayout
-				mainSectionHeading={"All announcements"}
-				// pointsEarned={"400"}
-				quickStartList={TEACHER_QUICK_START_LIST}
-				leftSidebarLinks={teacherLeftSidebarLinks()}
-			>
-				<div className="rounded-[2em] flex flex-col gap-[2em] w-full">
-					<div className="flex flex-col gap-[2em] w-full">
-						{previousAnnouncements.map((announcement, index) => (
-							<div className="flex flex-col w-full" key={index}>
-								<p className="text-[#555] text-[14px] font-semibold">
-									{announcement.date}
-								</p>
-								<p className="text-[#111] text-[18px] font-normal">
-									{announcement.announcement}
-								</p>
-							</div>
-						))}
-					</div>
-				</div>
-			</DashboardLayout>
-		</>
-	);
+  return (
+    <>
+      <DashboardLayout
+        mainSectionHeading={"All announcements"}
+        quickStartList={TEACHER_QUICK_START_LIST}
+        leftSidebarLinks={teacherLeftSidebarLinks()}
+      >
+        <div className="rounded-[2em] flex flex-col gap-[2em] w-full p-[2em] bg-gray-50">
+          <Announcements />
+        </div>
+      </DashboardLayout>
+    </>
+  );
+}
+
+function Announcements() {
+  const { user } = useCurrentUser();
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["fetchAnnouncements"],
+    queryFn: () => fetchAnnouncements(user?.school),
+  });
+  if (error) console.log(error);
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return <div>No announcements found</div>;
+
+  return (
+    <div className="flex flex-col w-full rounded-[1em] gap-[1.5em] p-[2em] bg-white shadow-lg">
+      <h3 className="uppercase text-[1.2em] font-semibold text-[#111]">
+        Previous Announcements
+      </h3>
+      <div className="flex flex-col gap-[2em] w-full">
+        {data?.map((announcement: any, index: number) => (
+          <div
+            className="flex flex-col w-full bg-gray-100 p-[1em] rounded-[1em] shadow-sm"
+            key={index}
+          >
+            <p className="text-[#555] text-[14px] font-semibold">
+              {formatDate(announcement.createdAt)}{" "}
+            </p>
+            <p className="text-[#111] text-[18px] font-normal">
+              {announcement.content}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
