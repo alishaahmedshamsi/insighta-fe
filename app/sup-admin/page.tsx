f99cce5ac8449480d92ead338fb391fb0a3e5fff
Moazam Ali <mozalikorai@gmail.com>
2024-07-15 00:55:31 +0500

    chore: Refactor school admin dashboard component

--- app/sup-admin/page.tsx
+++ app/sup-admin/page.tsx
@@ -87,42 +87,46 @@ export default function SchoolAdminDashboard() {
 					</h3>
 
 					<div className="rounded-[2em] grid grid-cols-2 gap-[2em]">
-						{data?.data.map((school, index) => (
-							<div
-								key={index}
-								className="flex justify-start items-center w-full bg-white rounded-[1em] gap-[1.5em] px-[1em] py-[1em]"
-							>
-								<div className="w-[80px]">
-									<div className="bg-gradient-to-b from-[#FB8397] to-[#B1CBF2] p-[.5em] w-[100%] rounded-[.5em] ">
-										<Image
-											alt=""
-											className="object-contain w-[5em] inline"
-											src={"/assets/building.png"}
-											width={600}
-											height={600}
-										/>
-									</div>
-								</div>
-								<div className="w-[75%]">
-									<h4 className="font-medium text-[#212121] align-middle text-[1.4em]">
-										{school.fullname}
-									</h4>
-									<p className="text-[#959BA5] text-[1em] align-middle">
-										{school.email}
-									</p>
-									<div className="flex gap-5 mt-2">
-										<div className="flex align-middle gap-2">
-											<User size={18} />
-											{school.studentCount}
-										</div>
-										<div className="flex gap-2">
-											<GraduationCapIcon size={18} />
-											{school.teacherCount}
+						{data && data.data.length == 0 ? (
+							<div>No schools.</div>
+						) : (
+							data?.data.map((school, index) => (
+								<div
+									key={index}
+									className="flex justify-start items-center w-full bg-white rounded-[1em] gap-[1.5em] px-[1em] py-[1em]"
+								>
+									<div className="w-[80px]">
+										<div className="bg-gradient-to-b from-[#FB8397] to-[#B1CBF2] p-[.5em] w-[100%] rounded-[.5em] ">
+											<Image
+												alt=""
+												className="object-contain w-[5em] inline"
+												src={"/assets/building.png"}
+												width={600}
+												height={600}
+											/>
 										</div>
 									</div>
+									<div className="w-[75%]">
+										<h4 className="font-medium text-[#212121] align-middle text-[1.4em]">
+											{school.fullname}
+										</h4>
+										<p className="text-[#959BA5] text-[1em] align-middle">
+											{school.email}
+										</p>
+										<div className="flex gap-5 mt-2">
+											<div className="flex align-middle gap-2">
+												<User size={18} />
+												{school.studentCount}
+											</div>
+											<div className="flex gap-2">
+												<GraduationCapIcon size={18} />
+												{school.teacherCount}
+											</div>
+										</div>
+									</div>
 								</div>
-							</div>
-						))}
+							))
+						)}
 					</div>
 				</div>
 			</DashboardLayout>
